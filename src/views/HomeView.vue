6873c795afb54b2e5e11c5374d338ed7eb4ca781
Julien Faure <julien.faure@iaca-electronique.com>
2024-09-17 14:39:50 +0200

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+    Hello world !
+</template>
+
+<script lang="ts">
+
+
+import {Component, Vue} from "vue-facing-decorator";
+
+@Component({
+})
+
+export default class HomeView extends Vue {
+
+
+
+    async mounted(): Promise<void> {
+
+    }
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
