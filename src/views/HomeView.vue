954880c68b2ae3827db013128aa8a0c86f8ecf70
Julien Faure <julien.faure@iaca-electronique.com>
2024-09-17 14:56:38 +0200

    Add HorlogeComponent to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,17 @@
 <template>
-    Hello world !
+    <main>
+        <horloge-component class="horloge"/>
+    </main>
 </template>
 
 <script lang="ts">
 
 
 import {Component, Vue} from "vue-facing-decorator";
+import HorlogeComponent from "@/components/HorlogeComponent.vue";
 
 @Component({
+    components: {HorlogeComponent}
 })
 
 export default class HomeView extends Vue {
@@ -23,5 +27,18 @@ export default class HomeView extends Vue {
 </script>
 
 <style scoped>
+
+main {
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+    justify-content: center;
+    align-items: center;
+}
+
+.horloge {
+    font-size: 8vw;
+    font-weight: normal;
+}
 
 </style>
