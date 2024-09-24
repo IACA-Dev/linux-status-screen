139d05a8e932d7010a9e3aa1fd6140b64054f2c4
Julien Faure <julien.faure@iaca-electronique.com>
2024-09-24 12:19:51 +0200

    Add VitalsService dependency to Context

--- src/Context.ts
+++ src/Context.ts
@@ -1,5 +1,19 @@
+import {VitalsService} from "@/services/VitalsService";
+import {isUndefined} from "@/utils/Types";
+import {VitalsServiceImpl} from "@/services/VitalsServiceImpl";
+import getEnv from "@/utils/Environment";
 
 export class Context {
 
+    private static readonly VITALS_API_URL: string = getEnv('VUE_APP_VITALS_API_URL')!;
+
+
+    private static vitalsService? : VitalsService;
+
+    public static provideVitalsService() : VitalsService {
+        if(isUndefined(this.vitalsService))
+            this.vitalsService = new VitalsServiceImpl(this.VITALS_API_URL);
+        return this.vitalsService!;
+    }
 
 }
